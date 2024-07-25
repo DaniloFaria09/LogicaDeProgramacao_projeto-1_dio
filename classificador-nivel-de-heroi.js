98e991124ccfa86bf45d88f06c38cdf5501b900a
DaniloFaria09 <danilofaria9@gmail.com>
2024-07-25 12:45:30 -0300

    primeiro commit

---
+++ classificador-nivel-de-heroi.js
@@ -0,0 +1,23 @@
+let nome = "Danilo"
+let nivel = 8000
+
+if (nivel <= 1000) {
+    console.log("O herói " + nome + " está no nivel FERRO!");
+}else if (nivel > 1000 && nivel <= 2000) {
+    console.log("O herói " + nome + " está no nivel BRONZE!");
+}else if (nivel > 2000 && nivel <= 5000) {
+    console.log("O herói " + nome + " está no nivel PRATA!");
+}else if (nivel > 5000 && nivel <= 7000) {
+    console.log("O herói " + nome + " está no nivel OURO!");
+}else if (nivel > 7000 && nivel <= 8000) {
+    console.log("O herói " + nome + " está no nivel PLATINA!");
+}else if (nivel > 8000 && nivel <= 9000) {
+    console.log("O herói " + nome + " está no nivel ASCENDENTE!");
+}else if (nivel > 9000 && nivel <= 10000) {
+    console.log("O herói " + nome + " está no nivel IMORTAL!");
+}else {
+    console.log("O herói " + nome + " está no nivel RADIANTE!");
+}
+    
+
+
